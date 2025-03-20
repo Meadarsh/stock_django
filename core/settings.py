e8b3a41af032af75b134bbec7a6892bdd6564c63
meadarsh <itsadarsh33@gmail.com>
2025-03-20 23:05:44 +0530

    fix

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,6 @@ PORT = os.getenv("PORT", "8342")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, "app", 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'app/static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build' 'static')
 
